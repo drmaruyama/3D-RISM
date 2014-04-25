992b1f0f733466e434fb53ce4c1de27d8a634913
Yutaka MARUYAMA <drmaruyama@gmail.com>
2014-04-25 16:26:46 +0900

    Delete src-cuda and src-mpi

--- src-mpi/set_fname.cc
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <fstream>
-#include "rism3d.h"
-
-void RISM3D :: set_fname (char inputfile[]) {
-  fname.append(inputfile);
-  fname = fname.substr(0, fname.rfind("."));
-}
--- src-mpi/set_solvent.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <fstream>
-#include "rism3d.h"
-
-void RISM3D :: set_solvent () {
-  if (myrank == 0) sv -> read(fsolvent);
-  sv -> setup_mpi(myrank);
-  sv -> spline(ga, indga, nga, ce -> mgrid);
-}
